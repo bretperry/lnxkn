e26c81d9048923b47963bd6751d43fe5247eaeaf
bretperry <73367922+bretperry@users.noreply.github.com>
2021-11-09 19:25:15 -0500

    added highlight functionality to list with autoscrolling; css edits

--- src/components/CommandTable.jsx
+++ src/components/CommandTable.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import styled from "@emotion/styled";
 import { observer } from "mobx-react";
-
+//import ReactDOM from 'react-dom';
 import store from "../store";
 
 import ItemRow from "../components/ItemRow";
@@ -14,38 +14,80 @@ const TableBase = styled.div`
 	height: 720px;
 	overflow-y:scroll;
 `;
+
+const tableTopMargin = 120;
 const Table = styled.table`
-	margin: 120px auto 650px;
+	margin-top: ${tableTopMargin+"px"}; 
+	margin-bottom: 650px;
 	background-color: ${cssVars.tableColor};
-	border: none;
 	font-weight:bold;
 	font-family: ${cssVars.fixedFont};
 	width: 100%;
 	color: ${cssVars.fontColor};
+	border-spacing: 0;
 `;
 const Thead = styled.thead`
 	display:none;
 `;
 const Tbody = styled.tbody`
 	background-color: #feffe7;
-	border: none;
+	border: 1px solid red;
 	font-weight:bold;
 	width: 100%;;
 `;
 
+const getBase = () => document.getElementById("scrollingBase");
+
 const CommandTable = () => {
+	//let selectEl = null;
+	//const base = document.getElementById("scrollingBase");
+	const hilit = {
+		elem: null,
+		distance: 0
+	}
 
 	const [chosen, setChosen] = useState();
-	const scrollTableTo = (item) => {
-		
+
+	const scrollTableTo = (item, ref) => {
+		let el = ref.current
+		//const tbod = el.parentElement;
+
 		//move so as to only query the dom once
 		const base = document.getElementById("scrollingBase");
+		//const table = base.childNodes[0];
+		//console.log(el, base, table, tbod);
+		// let distance = el.offsetTop - tableTopMargin;
+		let distance = el.offsetTop;
+		console.log("offset="+el.offsetTop, "D="+distance, "old scroll="+base.scrollTop);
 		
-		// let selectEl = ?? // just need the item's element!
-		// let distance = selectEl.offsetTop - selectEl.marginTop;
-		// base.scroll(distance, 0);
+		getBase().scrollTop=distance;
+		//debugger;
 	};
+	
 
+	// const setScrollData = (item, ref) => {
+	// 	hilit.elem = ref.current;
+	// 	hilit.distance = hilit.elem.offsetTop;
+	// };
+	
+
+
+	//getBase().scrollTop = hilit.elem.offsetTop;
+	
+  // useEffect(() => {
+
+  // 	// let base = getBase();
+  // 	// let offset = hilit.elem.offsetTop || 0;
+
+  // 	// base.scrollTop = offset;
+  // 	debugger;
+  // 	if (!!hilit.elem) getBase().scrollTop = hilit.elem.offsetTop
+
+  // });
+
+
+
+	
 	return(
 		<TableBase id="scrollingBase">
 		  <Table id="scrollingTable">
@@ -57,18 +99,24 @@ const CommandTable = () => {
 		    </Thead>
 		    <Tbody>
 		      {store.filteredList
-		        .map((item) =>(
-		          <ItemRow 
-		          	active={item === chosen}
-		            listItem={item} 
-		            key={item.name} 
-		            onSelect={(item) => {
-		            	store.setSelected(item);
-		            	setChosen(item);
-		            	scrollTableTo(item);
-		            }}
-		          />
-		     ))}
+		        .map((item) =>{
+		        	const ref = React.createRef();
+		        	return(
+			          <ItemRow 
+			          	active={item === chosen}
+			            listItem={item} 
+			            key={item.name} 
+			            ref={ref}
+			            onSelect={(item) => {
+			            	store.setSelected(item);
+			            	setChosen(item);
+			            	scrollTableTo(item, ref)
+			            	//hilit.elem = ref;
+			            }}
+			          />
+
+		     		)}
+		       	)}
 		    </Tbody>
 		  </Table>
 	  </TableBase>
--- src/components/ItemRow.jsx
+++ src/components/ItemRow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styled from "@emotion/styled";
 import cssVars from "../cssVars";
 
@@ -19,20 +19,59 @@ const Span = styled.span`
   margin: .3rem;
   color: ${cssVars.fontColorLt};
 `;
+//const ItemRow = ({ active, listItem, onSelect }) => {
+const ItemRow = React.forwardRef((props, ref) => {
+  //console.log(props, ref);
+  //debugger;
 
-const ItemRow = ({ active, listItem, onSelect }) => {
   return(
     <Row 
-      onClick={()=>onSelect(listItem)}
-      className={active ? "command active" : "command"}
+      ref={ref}
+      className={props.active ? "command active" : "command"}
+      onClick={()=>props.onSelect(props.listItem)}
     >
       <TdName>
-        {listItem.name}{console.log(listItem.name)} 
-        <Span>{listItem.type.join(", ")}</Span>
+        {props.listItem.name}
+        <Span>{props.listItem.type.join(", ")}</Span>
       </TdName>
     </Row>
-  );
-};
+  )});
+
+
+// const RowWithRef = React.forwardRef((props, ref )=>{
+//     <Row 
+//       onClick={()=>onSelect(listItem)}
+//       className={active ? "command active" : "command"}
+//     >
+//       <TdName>
+//         {listItem.name}{console.log(listItem.name)} 
+//         <Span>{listItem.type.join(", ")}</Span>
+//       </TdName>
+//     </Row>
+// });
+
+// const ItemRow = ({ ref, active, listItem, onSelect }) => {
+//   //const itemRef = useRef(null);
+//   return(
+//     <RowWithRef ref={ active, listItem, onSelect} />
+//   );
+// };
+
+ //Original without ref...
+
+// const ItemRow = ({ active, listItem, onSelect }) => {
+//   return(
+//     <Row 
+//       onClick={()=>onSelect(listItem)}
+//       className={active ? "command active" : "command"}
+//     >
+//       <TdName>
+//         {listItem.name}{console.log(listItem.name)} 
+//         <Span>{listItem.type.join(", ")}</Span>
+//       </TdName>
+//     </Row>
+//   );
+// };
 
 
 export default ItemRow;
